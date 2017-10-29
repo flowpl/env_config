c3b5755ef635d68c1ea8027c57e2b6afdfe0e2a4
Florian Plattner <florian.plattner@mylittlejob.de>
2017-10-29 18:16:11 +0100

    docs: rename README file

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name='env_config',
     packages=['env_config'],
-    version='1.0',
+    version='1.1',
     description='declare and load configuration from environment variables',
     author='Florian Plattner',
     author_email='me@florianplattner.de',
     url='https://github.com/flowpl/env_config',
-    download_url='https://github.com/flowpl/env_config/archive/1.0.tar.gz',
+    download_url='https://github.com/flowpl/env_config/archive/1.1.tar.gz',
     keywords=['config', 'configuration', 'environment', 'environment variables'],
     classifiers=[
 
