e246d5df26e611d97bffc62c537951fa895a4240
Florian Plattner <florian.plattner@mylittlejob.de>
2017-10-29 18:13:03 +0100

    fix: rename project once again

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-    name='config_py',
-    packages=['config_py'],
+    name='env_config',
+    packages=['env_config'],
     version='1.0',
     description='declare and load configuration from environment variables',
     author='Florian Plattner',
     author_email='me@florianplattner.de',
-    url='https://github.com/flowpl/config_py',
-    download_url='https://github.com/flowpl/config_py/archive/1.0.tar.gz',
+    url='https://github.com/flowpl/env_config',
+    download_url='https://github.com/flowpl/env_config/archive/1.0.tar.gz',
     keywords=['config', 'configuration', 'environment', 'environment variables'],
     classifiers=[
 
