5857892e8f5b6fa00f89879ad8fc50dce0d25a1f
Florian Plattner <florian.plattner@mylittlejob.de>
2017-10-29 18:21:24 +0100

    feat: switch to pbr based pypi publish workflow

--- setup.py
+++ setup.py
@@ -1,15 +1,2 @@
-from distutils.core import setup
-setup(
-    name='env_config',
-    packages=['env_config'],
-    version='1.1',
-    description='declare and load configuration from environment variables',
-    author='Florian Plattner',
-    author_email='me@florianplattner.de',
-    url='https://github.com/flowpl/env_config',
-    download_url='https://github.com/flowpl/env_config/archive/1.1.tar.gz',
-    keywords=['config', 'configuration', 'environment', 'environment variables'],
-    classifiers=[
-
-    ],
-)
+from setuptools import setup
+setup(setup_requires=['pbr>=3.1.1'], pbr=True)
